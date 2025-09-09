b07fe63af7b70d49a02d078c555c6366b4b1b48e
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-09 14:10:18 -0600

    added notes

---
+++ practices/string_notes.py
@@ -0,0 +1,12 @@
+#CP 7th String notes 
+
+#EXAMLES
+name = input("what is your name:\n").strip().title()
+
+last_name = input ("what is your last name:\n").strip().title()
+
+full_name =name,last_name
+
+sentence = 'the quick brown fox jumps over the lazy dog.'
+print(sentence.strip())
+print("welocme to my program",full_name=+"!")
