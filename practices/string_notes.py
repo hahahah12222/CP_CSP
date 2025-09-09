1e62d9205daccad4e969edba1b5283a8cfff0238
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-09 14:41:51 -0600

    i did notes

--- practices/string_notes.py
+++ practices/string_notes.py
@@ -1,12 +1,42 @@
 #CP 7th String notes 
 
-#EXAMLES
-name = input("what is your name:\n").strip().title()
+#1. What makes something a string?
+#a set of symbles that hold together by quotation marks
 
-last_name = input ("what is your last name:\n").strip().title()
+#Examples
+first_name = input("What is your name:\n").strip().title()
 
-full_name =name,last_name
+last_name = input("what is your last name:\n").strip().title()
 
-sentence = 'the quick brown fox jumps over the lazy dog.'
-print(sentence.strip())
-print("welocme to my program",full_name=+"!")
+full_name = first_name + " " + last_name
+sentence = 'The quick brown fox jumps over the lazy dog.'.strip()
+print(sentence.find("jumps"))
+print(sentence[20:25])
+print(sentence[sentence.find("lazy"): len("lazy")+sentence.find("lazy")])
+
+print("Welcome to my program", full_name + "!")
+
+#sanitization and stupid proofing
+
+#stupid proofing prevents the user to break down coding
+
+#debuging is fixing code
+
+#debug and bugs
+#bug 1 = sintax error = a mistake in your code
+#error = "this is a bug"
+#how do you get them? ny mispelling, forget a variable and uppercases
+#bug 2 = logic error 
+#numOne = "1"
+#numTwo = "2"
+
+#print(numOne + numTwo)
+ 
+ #run-time error
+ 
+# num = 3
+
+#run time error 
+
+#num=3
+#print("my favorit number is"+num)
