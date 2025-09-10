a1b7c594f14be8490ea706aeb26bb9129ed9b967
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-10 14:21:41 -0600

    i did assiment

--- practices/budget.calculator.py
+++ practices/budget.calculator.py
@@ -1,19 +1,20 @@
+#CP 7 budget.calc
+income = input("what is your monthly income:\n")
+rent = input("what is your monthly rent:\n")
+utilities = input("what is you monthly pay in utilities:\n")
+groceries = input("how much do you spend on groceries each month:\n")
+transportation = input("how much do you spend on transportation monthly:\n")
 
-print("Hello, this is a budget calculator")
-сл
- num_one = int(input("tell me your monthly income:\n"))
- num_two = int(input("tell me your rent:\n"))
-num_three = int(input("tell me your utilities cost:\n"))
-num_four = int(input("tell me how much your groceries cost:\n"))
-num_five = int(input("tell me how much your transportation cost:\n"))
-income num_one
-expenses = num_two + num_three+num_four + num_five
-print("Your income is:", income)
-savings income*.1
-print("You should put this much of your income into savings:", savings)
-spending income-(expenses+savings)
-print("you have this much spending money: ", spending)
-rent_percent (num_two/income) *100
-print("This percentage of your income goes to rent:", rent_percent) utilities_percent= (num_three/income)*100 print("This percentage of your income goes to utilities:", round(utilities_percent,1)) groceries_percent= (num_four/income)*100
-print("This percentage of your income goes to groceries:", round(groceries_percent)) transportation_percent= (num_five/income)*100
- print("This percentage of your income goes to transportation: ", round (transportation_percent))
+#turning strings to floats
+income_num = float(income)
+rent_num = float(rent)
+utilities_num = float(utilities)
+groceries_num = float(groceries)
+transportation_num = float(transportation)
+
+print("you pay $" + rent + " for rent each month, that is " + str(round((rent_num / income_num)*100, 2))+ "% of you icome")
+print("you pay $" + utilities + " for utilities each month, that is " + str(round((utilities_num / income_num)*100, 2))+ "% of you icome")
+print("you pay $" + groceries + " for groceries each month, that is " + str(round((groceries_num / income_num)*100, 2))+ "% of you icome")
+print("you pay $" + transportation + " for transportation each month, that is " + str(round((transportation_num / income_num)*100, 2))+ "% of you icome")
+print("you should save " + str(round(income_num * 0.1, 2)) + " each month. That is 10% of your income")
+print("you total spending money is $" + str(income_num - rent_num - groceries_num - transportation_num - utilities_num - (income_num * 0.1)))
