e73ab3d6928375a5c8c4a87e954477f3176e8443
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-15 13:58:46 -0600

    i did work

--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -1,1 +1,7 @@
 #CP 7 silly sentence 
+name = input("give me a name:\n").strip().title()
+verb = input("give me a past tense verb:\n")
+number = input("give me a any number:\n")
+print(name +
++ verb +
++ "from" + + number + + "meatballs.")
