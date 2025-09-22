22af229fd284e0a9944d018b8bc66f28ab3ae2da
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-22 14:41:48 -0600

    i did notes

--- practices/list.py
+++ practices/list.py
@@ -1,3 +1,14 @@
 #CP 7th list notes
 
-siblings=["alex,","katie","andrew",'vienna',"tia",]
+siblings=["alex,","katie","andrew",'vienna',"tia",
+    "treyson", "xavier","jake", ["joeseph","noah","zee"]]
+          
+print(siblings[1])
+print(siblings)
+siblings[0]="Eric"
+siblings[4]="vicoria"
+siblings[6]="jefferson"
+siblings[7]= "hailey"
+siblings.remove("vienna")
+print(siblings)
+
---
+++ practices/loops.py
@@ -0,0 +1,52 @@
+#CP 7 loops notes
+
+food=["chocolte","pasta","branies","cookies","garlic","bread","fajitas"]
+
+#for loop
+for food in food:
+ #print(f"{food} is my favorite food!")
+        print("hi!")
+
+for x in range(1,20):
+     print(x)
+
+#while loops
+i = 0
+while True:
+    print(i)
+    i+=1
+    #infinite loop (one of 3 steps missing)
+
+    #correct while loop
+    i = 1
+
+    while i < 20:
+      print(i)
+      i+=1
+
+
+      while x < 21:
+          if x % 2 == 0:
+               print(f"{x}is an even number.")
+          else:
+               print(f"{x}is an even number.")
+               x += 1
+
+               import random 
+
+               d = 1
+               end = random.ranint(1,20)
+
+               while d != end:
+                    print("duck")
+                    d== 1
+
+                    print("goose")
+
+                    while True:
+                         if d ==end:
+                              print("goose!")
+                              break
+                         else:
+                              print("duck")
+                              d +=1
