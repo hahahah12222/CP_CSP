65b63f33cb46547381f322fd34685cf18bd47905
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-23 14:29:31 -0600

    i did the work

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -1,1 +1,16 @@
 #CP 7 fizz buzz
+
+x = 1
+
+while x <=50:
+    if x % 3 == 0 and x % 5 ==0:
+        print("fizzbuzz")
+    elif x % 5 ==0:
+        print (("BUzz!"))
+    elif x % 3 == 0:
+        print("fizz")
+    else:
+        print(x)
+    x+=1
+print("thats all numbers through 50")
+
