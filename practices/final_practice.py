e630d2bba79561a060f8e8071b60b8ce124f64f8
hahahah12222 <ciara.payne@ucas_edu.net>
2025-09-25 14:41:30 -0600

    yeayea

---
+++ practices/final_practice.py
@@ -0,0 +1,19 @@
+#CP
+
+number = int(input("What's 5+2: "))
+if number == 7:
+    print("You are correct! keep going.")
+elif number <= 7:
+    print("you died.")
+elif number >= 7:
+    print("you died")
+
+
+    
+pais = input("De que pais eres?")
+if pais =="USA":
+    print("Correcto, sigue tu camino.")
+elif pais <= :
+    print("you died.")
+elif number >= 7:
+    print("you died")
