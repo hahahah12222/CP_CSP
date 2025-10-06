34deb3ccdd17e3bdbfd228b1967778ba4bd70a0d
hahahah12222 <ciara.payne@ucas-edu.net>
2025-10-06 14:31:42 -0600

    i did work

---
+++ C_programming/ecpression_notes.c
@@ -0,0 +1,21 @@
+// CP 7th expression notes
+
+#include <stdio.h>
+
+int main (boid){
+    int year=2025;
+    double long_pi = 3.14159265359;
+    printf("%d",year);
+    printf("%d\n", 8.0/3) ;
+    printf("%f\n",8/3);
+    printf("%d\n",(int) pow(2,4))
+
+
+    year +=1;//compound assiment operator
+    printf("%d",year);
+    printf("%d",year);
+    year++; //incrimentor    
+    printf("%d\n", year);
+
+    return 0;                                                                                               
+}
