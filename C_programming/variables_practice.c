ea7e536faba7402ca44d0cc090e297e51b2ae244
hahahah12222 <ciara.payne@ucas-edu.net>
2025-10-08 14:12:42 -0600

    i did the work

---
+++ C_programming/variables_practice.c
@@ -0,0 +1,31 @@
+//CP 7th varables practice
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+   
+    char name[] = "=Ciara";     
+    int luckyNumber = 2;                   
+    float gpa = 3.85;                       
+    char breakfast[] = "Pancakes and fruit"; 
+    char favoriteColor[] = "Blue";    
+    char schoolName[] = "Ucas";
+    int currentYear = 2025;                
+    char eyeColor[] = "Blue";            
+    int age = 14;
+    char favoriteSubject[] = "Advisory"; 
+
+    printf("Name: %s\n", name);
+    printf("Lucky Number (1-10): %d\n", luckyNumber);
+    printf("GPA: %.2f\n", gpa);
+    printf("Breakfast: %s\n", breakfast);
+    printf("Favorite Color: %s\n", favoriteColor);
+    printf("School Name: %s\n", schoolName);
+    printf("Current Year: %d\n", currentYear);
+    printf("Eye Color: %s\n", eyeColor);
+    printf("Age: %d\n", age);
+    printf("Favorite Subject: %s\n", favoriteSubject);
+
+    return 0;
+}
