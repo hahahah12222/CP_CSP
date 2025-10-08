9d753524b1cfd5ff04ff5b1b7e30854f5cc0b95d
hahahah12222 <ciara.payne@ucas-edu.net>
2025-10-08 14:25:08 -0600

    i did the work

---
+++ C_programming/ecpression_practice.c
@@ -0,0 +1,10 @@
+//CP 7th expresion practice 
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    printf("7-24/8*4+6= %d\n", 7-24/8*4+6);
+    printf("18/3-7+2*5= %d\n", 18/3-7+2*5);
+    printf("6*4/12+72/8-9= %d\n", 6)
+}
