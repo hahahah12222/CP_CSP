fab07447d1ec1515de69b84c90ca6b8d5ec0df67
hahahah12222 <ciara.payne@ucas-edu.net>
2025-10-08 14:14:41 -0600

    i did the work

--- C_programming/first_program.c
+++ C_programming/first_program.c
@@ -1,1 +1,10 @@
 //CP 7th first program
+#include <stdio.h>
+
+int main(void){
+char name[50];
+printf("What is your name:\n"); 
+scanf( "%s", name);
+printf("Hello, %s!! Welcome to your first C program.!\n ", name);
+return 0;
+}
