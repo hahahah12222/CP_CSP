a02c49016c95f6e0df10c2111aa17b13b86ceb06
hahahah12222 <ciara.payne@ucas-edu.net>
2025-10-09 18:21:21 -0600

    Wrote a Silly Sentence

--- C_programming/silly_sentences.c
+++ C_programming/silly_sentences.c
@@ -4,4 +4,18 @@
 #include <string.h>
 
 
-int main(void){}
+int main(void){
+    char adjective[25];
+    printf("Give me a adjective:\n");
+    scanf("%s", adjective);
+    
+    char noun[25];
+    printf("Give me a noun:\n");
+    scanf("%s",noun);
+
+    char verb[25];
+    printf("Give me a verb:\n");
+    scanf("%s", verb);
+
+    printf("The %s %s %sed loudly.\n", adjective, noun, verb);
+}
